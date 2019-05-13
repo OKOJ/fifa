85b8722ec1ff814d36be91986e53a6555187deee
OKOJ <aajmd@hotmail.com>
2019-05-12 18:06:13 -0600

    indexhandlebar

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,9 @@ if(process.env.JAWSDB_URL){
 }else {
   connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 8889,
   user: "root",
-  password: "Password",
+  password: "root",
   database: "fifa_db"
 
 });
